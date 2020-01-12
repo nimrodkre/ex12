79fc8c0c57f8e37158a571dce054869ee266dde2
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2020-01-12 15:39:19 +0200

    Started building functions

--- board_ui.py
+++ board_ui.py
@@ -1,2 +1,26 @@
 import tkinter
+import ttk
 
+class BoardUI:
+
+    def __init__(self, board):
+        self.__board = board
+        self.__buttons = []
+        self.__canvas = tkinter.Canvas()
+
+    @property
+    def board(self):
+        return self.__board
+
+    @property
+    def buttons(self):
+        return self.__buttons
+
+    @board.setter
+    def board(self, board):
+        self.__board = board
+
+    @buttons.setter
+    def buttons(self, buttons):
+        self.__buttons = buttons
+        
