d2d21cf1580c7711e305016661d1f5b10700df4e
tr3bi <omritre@gmail.com>
2020-01-13 15:27:41 +0200

    Starting to builf the Game manager

--- boggle.py
+++ boggle.py
@@ -1,5 +1,24 @@
+import boggle_board_randomizer
+from board_bl import BoardBL
+from board_ui import BoardUI
+from controller import Controller
 
 WORDS_FILE = "boggle_dict.txt"
+
+
+class BoggleGameManager:
+    def __init__(self, words):
+        self.__words = words
+
+    def start_game(self):
+        board = boggle_board_randomizer.randomize_board()
+        boggle_bl = BoardBL(board, self.__words)
+        controller = Controller(boggle_bl)
+        a = BoardUI(controller)
+
+        a.build_score()
+        a.build_buttons()
+        a.root.mainloop()
 
 
 def load_words():
@@ -9,3 +28,6 @@ def load_words():
 
 if __name__ == "__main__":
     print(len(load_words()))
+    words = load_words()
+    boggle_manager = BoggleGameManager(words)
+    boggle_manager.start_game()
