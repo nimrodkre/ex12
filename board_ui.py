adf8138faa0cc0e04e943494b3e9adfd33ae6e17
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2020-01-13 14:19:00 +0200

    Added all needed attributes

--- board_ui.py
+++ board_ui.py
@@ -11,6 +11,8 @@ class BoardUI:
         self.__quit = None
         self.__start = None
         self.__guess = None
+        self.__current_word = None
+        self.__undo = None
         self.__screen = tkinter.Tk()
 
     @property
@@ -40,6 +42,14 @@ class BoardUI:
     @property
     def guess(self):
         return self.__guess
+
+    @property
+    def current_word(self):
+        return self.__current_word
+
+    @property
+    def undo(self):
+        return self.__undo
 
     @property
     def root(self):
@@ -73,32 +83,58 @@ class BoardUI:
     def score(self, score):
         self.__score = score
 
+    @current_word.setter
+    def current_word(self, current_word):
+        self.__current_word = current_word
+
+    @undo.setter
+    def undo(self, undo):
+        self.__undo = undo
+
     @root.setter
     def root(self, root):
         self.__score = root
 
     def __button_callback(self, i, j):
         # for some reason not working
-        pass
+        print(i, j)
+
+    def make_callback(self, i, j):
+        return lambda: self.__button_callback(i, j)
 
     def build_buttons(self):
         for i in range(len(self.board)):
             self.buttons.append([tkinter.Button(self.root,
                                                 text=self.board[i][j],
-                                                command=lambda: self.__button_callback(
-                                                    i, j)) for j in
+                                                height=1, width=7,
+                                                command=self.make_callback(i,
+                                                                           j))
+                                 for j in
                                  range(len(self.board[0]))])
         for i in range(len(self.buttons)):
             for j in range(len(self.buttons[0])):
-                self.buttons[i][j].grid(row=i + 1, column=j + 1)
+                self.buttons[i][j].grid(row=i + 1, column=j)
 
     def build_score(self):
         self.score = tkinter.Text(self.root, height=1, width=10)
         self.score.insert(tkinter.INSERT, "Score=0")
-        self.score.grid(row=0, column=0)
+        self.score.grid(row=0, column=0, columnspan=4)
+
+    def build_current_word(self):
+        pass
+
+    def build_quit(self):
+        pass
+
+    def build_quess(self):
+        pass
+
+    def build_undo(self):
+        pass
 
 
-a = BoardUI([['a', 'b'], ['g', 'j']])
+a = BoardUI([['a', 'a', 'a', 'a'], ['a', 'a', 'a', 'a'], ['a', 'a', 'a', 'a'],
+             ['a', 'a', 'a', 'a']])
 a.build_score()
 a.build_buttons()
 a.root.mainloop()
