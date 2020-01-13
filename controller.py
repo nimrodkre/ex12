764acf636eb18ccca8680dde046eee5e3bc4cbcf
tr3bi <omritre@gmail.com>
2020-01-13 15:27:21 +0200

    Create dummy controller

--- controller.py
+++ controller.py
@@ -1,21 +1,16 @@
 class Controller:
-    def __init__(self, board_bl, board_ui):
+    def __init__(self, board_bl):
         self.__board_bl = board_bl
 
     @property
-    def board_bl(self):
-        return self.__board_bl
-
-    @property
-    def board_ui(self):
-        return self.__board_ui
-
-    @board_bl.setter
-    def board_bl(self, board_bl):
-        self.__board_bl = board_bl
+    def board(self):
+        return self.__board_bl.board
 
     def guess_word(self):
-        pass
+        return self.__board_bl.check_word()
+
+    def is_letter_valid(self, row, col, prev_row, prev_col):
+        return self.is_letter_valid(row, col, prev_row, prev_col)
 
     def quit_game(self):
         pass
