e814e0758eb925040146498e1e5769fd0c5abfcf
treb <omritre@gmail.com>
2020-01-19 16:49:06 +0200

    Add doc string to the main

--- boggle.py
+++ boggle.py
@@ -7,11 +7,19 @@ WORDS_FILE = "boggle_dict.txt"
 
 class BoggleGameManager:
     def __init__(self, words):
+        """
+        Creates a Boggle game orchestrator
+        :param words: A list of valid English words
+        """
         self.__words = words
         self.__ui = None
         self.__boggle_bl = None
 
     def start_game(self):
+        """
+        Start the crazy Boggle game
+        :return: None
+        """
         self.__boggle_bl = BoardBL(self.__words)
         controller = Controller(self.__boggle_bl)
         self.__ui = BoardUI(controller)
@@ -19,8 +27,12 @@ class BoggleGameManager:
         self.__ui.root.mainloop()
 
 
-def load_words():
-    with open(WORDS_FILE, 'r') as file:
+def load_words(words_file=WORDS_FILE):
+    """
+    Loads the words file
+    :return: A list of valid english words
+    """
+    with open(words_file, 'r') as file:
         return [line.strip() for line in file]
 
 
